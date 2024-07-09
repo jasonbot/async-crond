102719dc4c2b6ccd8a432c08dad2860662068127
Jason Scheirer <jason.scheirer@gmail.com>
2024-07-09 09:04:44 -0700

    Add comments as doc

--- crond.py
+++ crond.py
@@ -11,6 +11,9 @@ import crontab
 
 
 def run_code(callable):
+    """Correctly sniff out if the code provided is sync or not and run in
+    another thread if not.
+    """
     if inspect.iscoroutine(callable):
         asyncio.create_task(callable)
     elif inspect.iscoroutinefunction(callable):
@@ -19,11 +22,14 @@ def run_code(callable):
         asyncio.get_running_loop().run_in_executor(None, callable)
 
 
-async def schedule(schedule: str, function):
+async def schedule(schedule: str, callable):
+    """Very simple scheduler: set this, use `create_task`, and you can later
+    .cancel() it if it needs to stop.
+    """
     cron = crontab.CronTab(schedule)
     while True:
         await asyncio.sleep(cron.next(default_utc=True))
-        run_code(function)
+        run_code(callable)
 
 
 @dataclasses.dataclass(frozen=True, order=True)
@@ -47,18 +53,28 @@ class AsyncCron:
     async def _run(self):
         while self.alive:
             if self.schedule:
+                # Everythinfg runs in relation to when this code is first entered
                 now = datetime.datetime.now(tz=datetime.UTC)
+                # If the earliest scheduled job is set to run now or before...
                 while self.schedule and self.schedule[0].scheduled_time <= now:
+                    # Take it out of the schedule
                     cron_item = heapq.heappop(self.schedule)
+                    # Run its associated code
                     run_code(cron_item.callable)
 
+                    # Try to determine if it should be scheduled for another run
                     next_time = None
                     try:
-                        next_time = next(cron_item.next_time_iterator)
+                        # Ask for more values from iterator until we get one in the future
+                        while next_time := next(cron_item.next_time_iterator) <= now:
+                            pass
                     except StopIteration:
+                        # Iterator is exhausted, assume task it to stay out of schedule
                         pass
 
+                    # Is the task ready to be scheduled again?
                     if next_time is not None:
+                        # Add to schedule heap
                         heapq.heappush(
                             self.schedule,
                             (
@@ -71,6 +87,7 @@ class AsyncCron:
                             ),
                         )
 
+                # Determine how long to sleep until next scheduled task will be ready
                 time_to_sleep = 1.0
                 if self.schedule:
                     time_to_sleep = (
@@ -79,6 +96,9 @@ class AsyncCron:
                     if time_to_sleep < 0:
                         time_to_sleep = 0
 
+                # Wait for next scheduled task to be ready OR for the Event, which is a
+                # trigger letting us know the schedule has changed and the sleep value
+                # has potentially been invalidated
                 _, pending = await asyncio.wait(
                     [
                         asyncio.create_task(asyncio.sleep(time_to_sleep)),
@@ -86,13 +106,18 @@ class AsyncCron:
                     ],
                     return_when=asyncio.FIRST_COMPLETED,
                 )
+                # Cancel the sleep or the event.wait, depending on which other was
+                # the cause of the interruption
                 for pending_item in pending:
                     pending_item.cancel()
             else:
+                # Nothing in the schedule, sleep for a nominal amount of time
+                # so we don't space heater the CPU
                 await asyncio.sleep(1.0)
             self.event.clear()
 
     def add_to_schedule(self, cron_line, callable) -> uuid.UUID:
+        # Create an iterator that respects the CronTab's intervals
         def next_fn(cron: crontab.CronTab):
             while True:
                 yield datetime.datetime.now(tz=datetime.UTC) + datetime.timedelta(
@@ -101,20 +126,35 @@ class AsyncCron:
 
         next_iter = next_fn(crontab.CronTab(cron_line))
 
+        # Cronentry is a data strcuture that holds all the information needed
+        # for each scheduled item.
         entry: CronEntry = CronEntry(
+            # When it runs next
             scheduled_time=next(next_iter),
+            # How to determine when it runs next after that
             next_time_iterator=next_iter,
+            # What it calls
             callable=callable,
+            # An ID for later cancellation
             id=uuid.uuid4(),
         )
 
+        # Add item to schedule
         heapq.heappush(self.schedule, entry)
+        # Trigger the `asyncio.wait` in the self._run coroutine to stop
+        # via triggering the Event sync primitive
         self.event.set()
+
+        # Return a generated Id, which can be used for .cancel() later
         return entry.id
 
     def cancel(self, id: uuid.UUID):
+        # Remove item in schedule with target Id
         self.schedule = [item for item in self.schedule if item.id != id]
+        # Re-heapify poisoned list
         heapq.heapify(self.schedule)
+        # Trigger the `asyncio.wait` in the self._run coroutine to stop
+        # via triggering the Event sync primitive
         self.event.set()
 
 
